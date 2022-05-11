2478a3618b0af69e43822a47de19e55e0fe5e61d
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-05-11 11:45:35 +0800

    Update to v0.2

--- PrettyPTT.js
+++ PrettyPTT.js
@@ -2,101 +2,11 @@
 // @name         PrettyPTT
 // @description  讓網頁版PTT不再傷害你的眼睛。
 // @namespace    https://github.com/DonkeyBear/PrettyPTT
-// @version      0.1
+// @version      0.2
 // @author       DonkeyBear
 // @match        https://www.ptt.cc/bbs/*
 // @match        https://www.ptt.cc/man/*
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
-let colorPalette = {
-  gray_1: "#121212",
-  gray_2: "#181818",
-  gray_3: "#222222",
-  gray_4: "#323232",
-  gray_5: "#383838",
-  gray_6: "#4c4c4c",
-  gray_7: "#aaa",
-  blue_1: "#23315e",
-  blue_2: "#1e3771",
-  yellow_1: "ee8",
-  yellow_2: "#bdb643",
-  background: "#181818",
-  border: "",
-  text: "",
-  searchbar_background: "#121212"
-}
-
-let elmBody = document.body.style;
-let elmTopbar = document.querySelector("#topbar-container").style;
-let elmLogo = document.querySelector("#logo").style;
-
-let transparentElm = [];
-
-if (document.querySelectorAll("#action-bar-container").length !== 0) {
-  let elmActionBar = document.querySelector("#action-bar-container").style; // 上方功能按鈕區
-  transparentElm.push(elmActionBar);
-}
-if (document.querySelectorAll(".r-list-container").length !== 0) {
-  transparentElm.push(document.querySelector(".r-list-container").style);
-}
-if (document.querySelectorAll("#search-bar > .query").length !== 0) {
-  let elmSearchBar = document.querySelector("#search-bar > .query").style;
-}
-if (document.querySelectorAll(".r-list-sep").length !== 0) {
-  // 文章列表分隔線
-  let elmSeparate = document.querySelector(".r-list-sep").style;
-  elmSeparate.backgroundColor = colorPalette.gray_4;
-  elmSeparate.height = "2px";
-  elmSeparate.margin = "1.5ex 0";
-}
-
-if (document.querySelectorAll(".r-ent").length !== 0) {
-  // 個別文章區塊
-  for (let i of document.querySelectorAll(".r-ent")) {
-    i.style.backgroundColor = colorPalette.gray_2;
-  }
-}
-
-if (document.querySelectorAll(".b-list-container").length !== 0) {
-  transparentElm.push(document.querySelector(".b-list-container").style);
-}
-
-if (document.querySelectorAll(".bbs-screen") !== 0) {
-  transparentElm.push(document.querySelector(".bbs-screen").style);
-}
-
-if (document.querySelectorAll(".article-metaline").length !== 0) {
-  document.querySelector(".bbs-screen").style.lineHeight = 1.5;
-  document.querySelector(".article-metaline-right").style.lineHeight = "100%";
-  for (let i of document.querySelectorAll(".article-metaline")) {
-    i.style.lineHeight = "100%";
-    i.style.backgroundColor = colorPalette.blue_2;
-  }
-  for (let i of document.querySelectorAll(".article-meta-value")) {
-    i.style.backgroundColor = colorPalette.blue_2;
-  }
-  for (let i of document.querySelectorAll(".article-meta-tag")) {
-    i.style.color = colorPalette.blue_2;
-  }
-  if (document.querySelectorAll(".push-userid").length !== 0) {
-    for (let i of document.querySelectorAll(".push-userid")) {
-      i.style.color = colorPalette.yellow_1;
-    }
-  }
-  if (document.querySelectorAll(".push-content").length !== 0) {
-    for (let i of document.querySelectorAll(".push-content")) {
-      i.style.color = colorPalette.yellow_2;
-    }
-  }
-}
-
-if (transparentElm.length !== 0) {
-  for (let i of transparentElm) {
-    i.backgroundColor = "rgba(0, 0, 0, 0)";
-  }
-}
-
-elmBody.backgroundColor = colorPalette.gray_1;
-elmTopbar.backgroundColor = colorPalette.blue_1;
-elmLogo.color = colorPalette.yellow_1;
+GM_addStyle(':root{--color-gray-1:#121212;--color-gray-2:#181818;--color-gray-3:#222222;--color-gray-4:#323232;--color-gray-5:#383838;--color-gray-6:#4c4c4c;--color-gray-7:#aaa;--color-blue-1:#23315e;--color-blue-2:#1e3771;--color-yellow-1:#bdb643;--color-yellow-2:#ee8}#action-bar-container,.b-list-container,.bbs-screen,.r-list-container{background-color:transparent}body{background-color:var(--color-gray-1)}#logo{color:var(--color-yellow-2)}#topbar-container{background-color:var(--color-blue-1)}.r-list-sep{background-color:var(--color-gray-4);height:2px;margin:1.5ex 0}.r-ent{background-color:var(--color-gray-2)}.bbs-screen{line-height:1.5}.article-metaline,.article-metaline-right{line-height:100%}.article-meta-value,.article-metaline{background-color:var(--color-blue-2)}.article-meta-tag{color:var(--color-blue-2)}.push-userid{color:var(--color-yellow-2)}.push-content{color:var(--color-yellow-1)}');
